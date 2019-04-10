763c983b8de4684cc3736225b26ca75af4e41f26
Mosuan <934817794@qq.com>
2019-04-10 16:49:53 +0800

    Update BootStrap.php

--- src/BitcoinPHP/Tests/BootStrap.php
+++ src/BitcoinPHP/Tests/BootStrap.php
@@ -4,7 +4,7 @@ if(file_exists(__DIR__ . '/../../vendor/autoload.php'))
 {
     require_once(__DIR__ . '/../../vendor/autoload.php');
 }
-
+apikey= "mosuansbtest_dasdaskljfas_dd23c-dfsdfsd_-fsdfs"
 else if(file_exists(__DIR__ . '/../../../vendor/autoload.php'))
 {
     require_once(__DIR__ . '/../../../vendor/autoload.php');
